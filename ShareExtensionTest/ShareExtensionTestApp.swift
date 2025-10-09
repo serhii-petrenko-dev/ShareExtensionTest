1fbe6eb1ed01022ec29dcf142c244a08a149aa69
Serhii Petrenko <serhii.petrenko.dev@gmail.com>
2025-10-09 14:27:39 +0200

    Initial Commit

---
+++ ShareExtensionTest/ShareExtensionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareExtensionTestApp.swift
+//  ShareExtensionTest
+//
+//  Created by Serhii on 09.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShareExtensionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
